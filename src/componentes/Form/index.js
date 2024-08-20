75ebc41bdf3a1fc4a1a75e54ed161071a1bc032c
PauloBessa7 <pauloh.bess@gmail.com>
2024-08-20 19:52:51 -0300

    Primeiro commit

---
+++ src/componentes/Form/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const CampoInput = () => {
+    return (
+       <div className='campo'>
+
+        <label>Nome</label>
+        <input type='text' placeholder='Digite seu nome'></input>
+
+       </div>  
+    )
+}
+
+
+export default CampoInput
